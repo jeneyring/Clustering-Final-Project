3f09c3fdb7524100e7c58990a76d2eef5efe04af
“Jen <Jen.j.eyring@gmail.com>
2022-06-14 17:01:19 -0500

    going

--- wrangle.py
+++ wrangle.py
@@ -55,22 +55,48 @@ def get_zillow_data():
         df.to_csv('zillow.csv', index=False)
     return df
 
+def df_summary(df):
+    print("---Shape: {}".format(df.shape))
+    print()
+    print('---Info')
+    df.info()
+    print()
+    print('--- Column Description')
+    print(df.describe(include='all'))
+#####______________________________________
+#how to look at count and percentage of nulls per column
 def nulls_by_columns(df):
     return pd.concat([
         df.isna().sum().rename('count'),
         df.isna().mean().rename('percent')
     ], axis=1)
 
+#how to look at count and percentage of nulls per row
 def nulls_by_rows(df):
     return pd.concat([
         df.isna().sum(axis=1).rename('n_missing'),
         df.isna().mean(axis=1).rename('percent_missing'),
     ], axis=1).value_counts().sort_index()
 
-def handle_missing_values(df, prop_required_column, prop_required_row):
-    n_required_column = round(df.shape[0] * prop_required_column)
-    n_required_row = round(df.shape[1] * prop_required_row)
-    df = df.dropna(axis=0, thresh=n_required_row)
-    df = df.dropna(axis=1, thresh=n_required_column)
+#####______________________________________
+#removing columns not needed/wanted/replicated
+def remove_columns(df, cols_to_remove):
+    df = df.drop(columns=cols_to_remove)
     return df
+
+#How to handle missing values based on minimum percentage of values 
+#for rows and columns
+def handle_missing_values(df, prop_required_column = .5, prop_required_row = .75):
+    threshold = int(round(prop_required_column*len(df.index),0))
+    df.dropna(axis=1, thresh=threshold, inplace=True)
+    threshold = int(round(prop_required_row*len(df.columns),0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    return df
+#####_______________________________________
+#PUTTING ABOVE TWO FUNCTIONS TOGETHER:
+def data_prep(df, cols_to_remove=[], prop_required_column=.5, prop_required_row=.75):
+    df = remove_columns(df, cols_to_remove)
+    df = handle_missing_values(df, prop_required_column, prop_required_row)
+    return df
+
 
