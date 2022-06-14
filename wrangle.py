4f648556c84a54ec12611c2e66c437ca94f8d2d6
“Jen <Jen.j.eyring@gmail.com>
2022-06-14 15:02:12 -0500

    going

---
+++ wrangle.py
@@ -0,0 +1,76 @@
+"""
+This file holds the functions that pair along the Zillow Clustering Final Project
+by Jen Eyring, Jemison-cohort.
+
+These functions are for:
+-Acquiring the dataset from MySQL
+-Preparing the dataset through handling nulls, dropping columns, etc
+-Split the dataset into Train, Validate, Test
+
+"""
+
+#imports for functions to work:
+import os
+import pandas as pd
+import env
+
+sql = """SELECT
+        p.*,
+        predictions_2017.logerror,
+        predictions_2017.transactiondate,
+        a.airconditioningdesc,
+        arch.architecturalstyledesc,
+        build.buildingclassdesc,
+        heat.heatingorsystemdesc,
+        landuse.propertylandusedesc,
+        story.storydesc,
+        construct.typeconstructiondesc
+    FROM properties_2017 AS p
+    JOIN (
+        SELECT parcelid, MAX(transactiondate) AS max_transactiondate
+        FROM predictions_2017
+        GROUP BY parcelid
+    ) pred USING(parcelid)
+    JOIN predictions_2017 ON pred.parcelid = predictions_2017.parcelid
+                          AND pred.max_transactiondate = predictions_2017.transactiondate
+    LEFT JOIN airconditioningtype AS a USING (airconditioningtypeid)
+    LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid)
+    LEFT JOIN buildingclasstype build USING (buildingclasstypeid)
+    LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid)
+    LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid)
+    LEFT JOIN storytype story USING (storytypeid)
+    LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid)
+    WHERE p.latitude IS NOT NULL
+      AND p.longitude IS NOT NULL
+      AND transactiondate <= '2017-12-31'
+      AND propertylandusedesc = "Single Family Residential"
+"""
+def get_zillow_data():
+    if os.path.exists('zillow.csv'):
+        df = pd.read_csv('zillow.csv')
+    else:
+        database = 'zillow'
+        url = f'mysql+pymysql://{env.username}:{env.password}@{env.host}/{database}'
+        df = pd.read_sql(sql, url)
+        df.to_csv('zillow.csv', index=False)
+    return df
+
+def nulls_by_columns(df):
+    return pd.concat([
+        df.isna().sum().rename('count'),
+        df.isna().mean().rename('percent')
+    ], axis=1)
+
+def nulls_by_rows(df):
+    return pd.concat([
+        df.isna().sum(axis=1).rename('n_missing'),
+        df.isna().mean(axis=1).rename('percent_missing'),
+    ], axis=1).value_counts().sort_index()
+
+def handle_missing_values(df, prop_required_column, prop_required_row):
+    n_required_column = round(df.shape[0] * prop_required_column)
+    n_required_row = round(df.shape[1] * prop_required_row)
+    df = df.dropna(axis=0, thresh=n_required_row)
+    df = df.dropna(axis=1, thresh=n_required_column)
+    return df
+
